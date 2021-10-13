6f64409f82631df09962d8335bb35f3555eda17a
Bee Workeneh <84352386+bworkeneh-IDM@users.noreply.github.com>
2021-10-13 12:53:00 -0700

    updates to requirements.txt, about.py, gene_drive_greeting.py

--- Gene_Drive/components/about.py
+++ Gene_Drive/components/about.py
@@ -1,7 +1,151 @@
 from dash import html
 
 about = html.Div(
-    className="content", children=[
-    html.H4(className="title titleText", children="What is Gene Drive?"),
-    html.P(children="Gene Drive is ..."),
-])
+    className="content",
+    children=[
+        html.H4(className="title titleText", children="Overview"),
+        html.P(
+            children="""
+            Using this website, you can explore the effects of a single 
+            release of gene drive mosquitoes with different values of 
+            various gene drive system and vector population parameters 
+            on malaria elimination probabilities and prevalence, as well 
+            as vector populations and genetics. Eight year-long simulations 
+            of population replacement gene drive mosquito release were run 
+            using EMOD 2.20 in a highly seasonal Sahelian setting over a 
+            300 square kilometer region. Here elimination is defined as 
+            occurring when malaria prevalence reaches zero by the end of 
+            simulation year 7 and remains at zero through the end of simulation 
+            year 8 across the entire spatial region.
+            """
+        ),
+        html.P(
+            children=[
+                html.Span(
+                    children="The parameters that can be explored here include the following:"
+                ),
+                html.Ul(
+                    children=[
+                        html.Li(
+                            children="The probability of copying over the driver and/or "
+                                     "effector genes when the driver gene is present (also "
+                                     "known as drive efficiency; d in the classic case, d1 "
+                                     "in the integral case)"
+                        ),
+                        html.Li(
+                            children="The ability of the introduced effector gene to suppress "
+                                     "malaria transmission in mosquitoes (also known as the "
+                                     "phenotypic effectiveness of the drive, rc)"
+                        ),
+                        html.Li(
+                            children="The initial frequency of target site resistant alleles "
+                                     "in the population (rr0 in the classic case, rr20 at the "
+                                     "effector target site in the integral case)"
+                        ),
+                        html.Li(
+                            children="The fitness cost associated with expressing the introduced "
+                                     "driver and effector genes, represented by an increase in "
+                                     "vector mortality (sne in the classic case, se2 associated "
+                                     "with the effector in the integral case)."
+                        )
+                    ]
+                )
+            ]
+        ),
+        html.P(
+            children=[
+                html.Span(
+                    children="The paper associated with these visualizations and results can be found here: "
+                ),
+                html.A(
+                    href="",
+                    target="_blank",
+                    children="<CITATION>. "
+                ),
+                html.Span(
+                    children="Any questions can be sen to "
+                ),
+                html.A(
+                    href="mailto:support@idmod.org",
+                    children="support@idmod.org"
+                ),
+                html.Span(".")
+
+            ]
+        ),
+        html.H4(className="title titleText", children="Motivation"),
+        html.P(
+            children="""
+                    Malaria remains a significant health burden in Sub-Saharan Africa 
+                    (SSA) despite many decades of effort to eliminate the disease. 
+                    However, concerns about drug and insecticide resistance threaten 
+                    to stall traditional malaria control efforts. New strategies and 
+                    technologies will therefore be needed to achieve elimination in 
+                    SSA. Releases of gene drive mosquitoes are one potentially 
+                    low-cost, long-lasting, and effective new strategy. Mosquitoes 
+                    engineered with gene drive systems can copy specified genes from 
+                    one chromosome to another in germline cells, ensuring that these 
+                    genes are passed onto their offspring at higher than Mendelian 
+                    inheritance rates and therefore rapidly spread through a population 
+                    even if there are associated fitness costs.
+                    """
+        ),
+        html.H4(className="title titleText", children="Gene Drive System Details"),
+        html.P(
+            children="""    
+                    The homing endonuclease gene drive systems considered here consist 
+                    of a driver gene that enables the copying of both itself and an 
+                    “effector” gene, which in turn confers desired phenotypic traits. 
+                    The driver gene encodes a guide RNA and an endonuclease, such as 
+                    Cas9, that together recognize and cut specified DNA sequences 
+                    present in the wildtype mosquito population. Within mosquitoes 
+                    that are heterozygous in wildtype and drive or effector alleles, 
+                    the cut wildtype chromosome uses its intact drive or 
+                    effector-containing sister chromosome as a template for repairing 
+                    itself, copying over the intact chromosome’s drive or 
+                    effector-containing DNA in the process through homology-directed 
+                    repair (HDR).
+                    """
+        ),
+        html.P(
+            children="""
+                    Depending on which effector genes are introduced, gene drive mosquito 
+                    releases can either reduce (population suppression) or modify 
+                    (population replacement) a given vector population. Population 
+                    replacement rather than suppression may be desirable in locations 
+                    where the ecological effects of removing a mosquito species are not 
+                    well known. For population replacement applied to malaria reduction 
+                    or elimination, many potential effector genes have been shown to 
+                    prevent malaria infection and/or transmission in Anopheles mosquitoes. 
+                    These include genes that code for immune system activators, peptides 
+                    that neutralize Falciparum parasites in the mosquito midgut or salivary 
+                    glands, and others. We focus on population replacement strategies here.
+                    """
+        ),
+        html.H4(className="title titleText",
+                children="Important Gene Drive System and Vector Population Parameters to Explore"
+        ),
+        html.P(
+            children="""
+                    An important question that arises is how well effector genes must work in 
+                    order to deliver substantial reductions in malaria transmission. 
+                    Even without perfect blocking of malaria transmission within each engineered 
+                    mosquito, can elimination be achieved? If there are significant fitness 
+                    costs associated with expressing the effector, will it still be able to 
+                    spread quickly through the population? Questions also arise around the 
+                    required efficiency of the driver gene and gene drive system itself to 
+                    achieve elimination. The process of copying the effector gene from one 
+                    chromosome to another is not always successful. After cutting, DNA can 
+                    sometimes undergo alternative repair pathways that do not result in 
+                    accurate copying of the drive or effector-containing DNA on the sister 
+                    chromosome, potentially generating “resistant” alleles that do not 
+                    contain the desired drive or effector gene but are no longer recognized 
+                    by the driver endonuclease. These resistant alleles may also be present 
+                    in the wild mosquito population even before introduction of new drive or 
+                    effector genes. The extent to which the generation and initial presence 
+                    of these resistant alleles affects the ability of introduced genes to spread 
+                    through the population via gene drive must be better quantified.
+                    """
+        )
+
+    ])
--- Gene_Drive/components/gene_drive_greeting.py
+++ Gene_Drive/components/gene_drive_greeting.py
@@ -4,6 +4,7 @@ from dash import html, dcc, callback
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
+
 class GeneDriveGreetingAIO(html.Div):
     def __init__(self):
         super().__init__([
@@ -16,20 +17,79 @@ class GeneDriveGreetingAIO(html.Div):
                                 dbc.Jumbotron(
                                     className="m-0 p-2",
                                     children=[
-                                        html.H1("Gene Drive", className="display-3"),
+                                        html.H1("Gene Drive", className="display-4"),
+                                        html.H5(
+                                            children="Population replacement gene drives for "
+                                                     "malaria elimination in a highly seasonal Sahelian setting"
+                                        ),
                                         html.P(
-                                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                                            " Nam et augue nisi. Morbi finibus, tellus a congue accumsan,"
-                                            " odio felis dictum orci, nec feugiat elit tellus tempus massa."
-                                            " Quisque turpis est, sagittis nec euismod eget, dignissim et augue. "
-                                            "Curabitur a mattis turpis. Aenean mattis ligula vel purus mollis "
-                                            "rutrum. Nullam vitae nibh eu erat congue faucibus. Pellentesque "
-                                            "pellentesque, ligula cursus rutrum rutrum, dolor tellus pharetra "
-                                            "velit, id porta dui elit non mi. Fusce congue lacinia commodo. "
-                                            "Nam pulvinar congue arcu, sed varius tellus tincidunt ac. "
-                                            "Mauris lorem sapien, elementum eu lacinia sed, tempus sit "
-                                            "amet elit. Nam laoreet molestie erat, eget commodo felis luctus ut.",
-                                            className="lead",
+                                            children="""
+                                                    Using this website, you can explore the effects of a single release 
+                                                    of gene drive mosquitoes with different values of various gene 
+                                                    drive system and vector population parameters on malaria 
+                                                    elimination probabilities and prevalence, as well as vector 
+                                                    populations and genetics. Eight year-long simulations of 
+                                                    population replacement gene drive mosquito release were run using 
+                                                    EMOD 2.20 in a highly seasonal Sahelian setting over a 300 square 
+                                                    kilometer region. Here elimination is defined as occurring when 
+                                                    malaria prevalence reaches zero by the end of simulation year 7 
+                                                    and remains at zero through the end of simulation year 8 across 
+                                                    the entire spatial region.
+                                                    """
+                                        ),
+                                        html.P(
+                                            children=[
+                                                html.Span(
+                                                    children="The parameters that can be explored here include the following:"
+                                                ),
+                                                html.Ul(
+                                                    children=[
+                                                        html.Li(
+                                                            children="The probability of copying over the driver and/or "
+                                                                     "effector genes when the driver gene is present (also "
+                                                                     "known as drive efficiency; d in the classic case, d1 "
+                                                                     "in the integral case)"
+                                                        ),
+                                                        html.Li(
+                                                            children="The ability of the introduced effector gene to suppress "
+                                                                     "malaria transmission in mosquitoes (also known as the "
+                                                                     "phenotypic effectiveness of the drive, rc)"
+                                                        ),
+                                                        html.Li(
+                                                            children="The initial frequency of target site resistant alleles "
+                                                                     "in the population (rr0 in the classic case, rr20 at the "
+                                                                     "effector target site in the integral case)"
+                                                        ),
+                                                        html.Li(
+                                                            children="The fitness cost associated with expressing the introduced "
+                                                                     "driver and effector genes, represented by an increase in "
+                                                                     "vector mortality (sne in the classic case, se2 associated "
+                                                                     "with the effector in the integral case)."
+                                                        )
+                                                    ]
+                                                )
+                                            ]
+                                        ),
+                                        html.P(
+                                            children=[
+                                                html.Span(
+                                                    children="The paper associated with these visualizations and results can be found here: "
+                                                ),
+                                                html.A(
+                                                    href="",
+                                                    target="_blank",
+                                                    children="<CITATION>. "
+                                                ),
+                                                html.Span(
+                                                    children="Any questions can be sen to "
+                                                ),
+                                                html.A(
+                                                    href="mailto:support@idmod.org",
+                                                    children="support@idmod.org"
+                                                ),
+                                                html.Span(".")
+
+                                            ]
                                         ),
                                         html.Hr(className="my-2"),
                                         html.P(
@@ -58,8 +118,7 @@ class GeneDriveGreetingAIO(html.Div):
                     size='xl'
                 ),
             ]
-        )])
-
+            )])
 
     @callback(
         Output("greeting-modal", "is_open"),
@@ -71,6 +130,6 @@ class GeneDriveGreetingAIO(html.Div):
             print("setting cookie 'greeting-modal-shown' True")
             return False
         all_cookies = dict(flask.request.cookies)
-        if "greeting-modal-shown" in all_cookies and all_cookies["greeting-modal-shown"]== "True":
+        if "greeting-modal-shown" in all_cookies and all_cookies["greeting-modal-shown"] == "True":
             return False
         return True
